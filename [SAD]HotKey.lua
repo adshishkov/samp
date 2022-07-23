5247a73c2c47f7e5787fa29bbb6bf89d6e53e5e9
Arthur Shishkov <109356892+adshishkov@users.noreply.github.com>
2022-07-24 01:03:19 +0300

    modified:   [SAD]HotKey.lua

--- [SAD]HotKey.lua
+++ [SAD]HotKey.lua
@@ -9,21 +9,25 @@ function main()
     while not isSampAvailable() do wait(100) end
     while true do
     wait(0)
+        -- Lock Car
         if not sampIsChatInputActive() and not sampIsDialogActive() and not isPauseMenuActive() and not isSampfuncsConsoleActive() and not sampIsCursorActive() then
             if wasKeyPressed(key.VK_L) then
             sampSendChat('/lock')
             end
         end
+        -- Key Car
         if not sampIsChatInputActive() and not sampIsDialogActive() and not isPauseMenuActive() and not isSampfuncsConsoleActive() and not sampIsCursorActive() then
             if wasKeyPressed(key.VK_K) then
             sampSendChat('/key')
             end
         end
+        -- Bring down Anim
         if not sampIsChatInputActive() and not sampIsDialogActive() and not isPauseMenuActive() and not isSampfuncsConsoleActive() and not sampIsCursorActive() then
             if wasKeyPressed(key.VK_1) then
             if not isCharInAnyCar(PLAYER_PED) then clearCharTasksImmediately(PLAYER_PED) setPlayerControl(playerHandle, 1) freezeCharPosition(PLAYER_PED, false) end
             end
         end
+        -- Time
         if not sampIsChatInputActive() and not sampIsDialogActive() and not isPauseMenuActive() and not isSampfuncsConsoleActive() and not sampIsCursorActive() then
             if wasKeyPressed(key.VK_X) then
             sampSendChat('/time')
@@ -31,6 +35,12 @@ function main()
             sampSendChat('/me приподн€в руку смотрит на часы [Apple Watch 7]')
             wait(1000)
             sampSendChat("/do —ейчас на часах: " .. os.date("%X"))
+            end
+        end
+        -- Mask
+        if not sampIsChatInputActive() and not sampIsDialogActive() and not isPauseMenuActive() and not isSampfuncsConsoleActive() and not sampIsCursorActive() then
+            if wasKeyPressed(key.VK_M) then
+            sampSendChat('/mask')
             end
         end
     end
